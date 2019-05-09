5b8d3c8a0661a2404330a043aa8837b20831a2ee
bdistin <bdistin@gmail.com>
2019-05-09 16:05:23 -0500

    remove concat, it's too opinionated
    can easily be replicated with Array#join(whatever)

--- src/concat.ts
+++
@@ -1,3 +0,0 @@
-export default function concat(...args: any[]): string {
-    return `${args.shift()}${args.length ? ` ${concat(...args)}` : ''}`;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { default as bold } from './bold';
 export { default as code } from './code';
 export { default as codeblock } from './codeblock';
-export { default as concat } from './concat';
 export { default as italic } from './italic';
 export { default as nest } from './nest';
 export { default as spoiler } from './spoiler';
